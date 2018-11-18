ad8a0ac9b630a93901aa7de594e37885b946617d
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-18 20:36:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
+require 'shotgun'
 
 class App < Sinatra::Base
+
+  get '/name' do "My name is Paul"
+  end
+  get '/hometown' do "My hometown is rolling stone gathers no moss"
+  end
+  get '/favorite-song' do "My favorite song is Happy Birthday"
+  end
 end
