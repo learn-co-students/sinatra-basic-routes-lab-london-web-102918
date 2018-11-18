6fb4dfad5c9dfe0ffab61f5f106ca2e3d50a8921
rosen777 <rosen.toshev@flatironschool.com>
2018-11-18 21:06:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get ('/') {'Hello, World!'}
+  get ('/name') {'My name is Rosen'}
+  get ('/hometown') {'My hometown is Sofia'}
+  get ('/favorite-song') {'My favorite song is Viva La Vida'}
 end
